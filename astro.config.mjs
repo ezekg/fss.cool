601a27c88e69a4dad4a777c0b439fa8753eec411
Zeke Gabrielse <ezekg@yahoo.com>
2024-09-26 12:13:17 -0500

    add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fss.cool',
   integrations: [
     react({
       experimentalReactChildren: true,
