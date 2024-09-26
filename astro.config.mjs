7a78ce981df65ea6924e10ef61a2c39214f83a19
Zeke Gabrielse <ezekg@yahoo.com>
2024-09-26 13:24:45 -0500

    fix format

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fss.cool',
+  site: "https://fss.cool",
   integrations: [
     react({
       experimentalReactChildren: true,
